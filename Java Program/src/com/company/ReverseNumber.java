5cdf26c3639c5277e28fe056a27861fa07a305da
Harshit <gharshit89@gmail.com>
2021-12-18 21:34:25 +0530

    updated

--- Java Program/src/com/company/ReverseNumber.java
+++ Java Program/src/com/company/ReverseNumber.java
@@ -3,17 +3,17 @@ package com.company;
 public class ReverseNumber {
     public static void main(String[] args) {
         int n=28479;
-//        while(n>0){
-//            int rem=n%10;
-//            System.out.print(rem);
-//            n=n/10;
-//        }
-        int ans =0;
         while(n>0){
             int rem=n%10;
-            n/=10;
-            ans=ans*10+rem;
+            System.out.print(rem);
+            n=n/10;
         }
-        System.out.println(ans);
+//        int ans =0;
+//        while(n>0){
+//            int rem=n%10;
+//            n/=10;
+//            ans=ans*10+rem;
+//        }
+//        System.out.println(ans);
     }
 }
