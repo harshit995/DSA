8ea42f10fc712a202579fb0bd64d1d08baa0757c
Harshit <gharshit89@gmail.com>
2021-12-18 21:40:42 +0530

    update

--- Java Program/src/com/company/Switch.java
+++ Java Program/src/com/company/Switch.java
@@ -10,6 +10,11 @@ public class Switch {
 
         if(fruit.equals("mango")){
             System.out.println("King of Fruits");
+        } else if(fruit.equals("Apple")){
+            System.out.println("Keeps the doctor away");
+        }
+        else{
+            System.out.println("invalid fruit");
         }
     }
 }
