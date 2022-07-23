f8fab7a6e7b68897c93936145b7c5fc7a7963169
Harshit <gharshit89@gmail.com>
2022-07-23 18:35:41 +0530

    updated

---
+++ Java Program/src/com/company/DSA/Hashing/FrequencyEle.java
@@ -0,0 +1,24 @@
+package com.company.DSA.Hashing;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FrequencyEle {
+    static int freqEle(int[] arr,int n){
+        HashMap <Integer,Integer> h =new HashMap<>();
+        for (int x:arr
+             ) {
+            h.put(x,h.getOrDefault(x,0)+1);
+        }
+        for (Map.Entry<Integer,Integer> e: h.entrySet()){
+            System.out.println(e.getKey()+" "+ e.getValue());
+
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        int[] arr={6,5,6,6,2,3};
+        int n=arr.length;
+        System.out.println(freqEle(arr,n));
+    }
+}
