5cc8a78dbe8c8416d1b86bcbfecdb6cd937f695f
Harshit <gharshit89@gmail.com>
2022-07-28 21:26:04 +0530

    updated

--- Java Program/src/com/company/DSA/LinkedList/InsertAtEnd.java
+++ Java Program/src/com/company/DSA/LinkedList/InsertAtEnd.java
@@ -1,4 +1,32 @@
 package com.company.DSA.LinkedList;
 
 public class InsertAtEnd {
+static class Node{
+    int data;
+    Node next;
+    Node(int x){
+        data=x;
+        next=null;
+    }
+
+    static Node insertEnd(Node head,int a){
+        Node temp=new Node(a);
+        if(head==null){
+            return temp;
+        }
+        else{
+            Node curr =head;
+            while(curr.next!=null){
+                curr=curr.next;
+            }
+            curr.next=temp;
+            return head;
+        }
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next=new Node(20);
+        System.out.println( insertEnd(head,52));
+    }
+}
 }
