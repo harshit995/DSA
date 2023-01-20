260f286efbc25d82a1a09cc43c60d379d66fb4f1
Harshit <gharshit89@gmail.com>
2023-01-21 02:55:24 +0530

    updated

---
+++ Java Program/src/com/company/DSA/Intersection.java
@@ -0,0 +1,43 @@
+package com.company.DSA;
+import java.util.*;
+public class Intersection {
+
+
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+
+    public static boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int arr1[]={8,9,22,12};
+        int arr2[]={8,5};
+        System.out.println("elements are" + intersection(arr1,arr2));
+    }
+}
