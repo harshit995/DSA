629aac1ad823326107f2884687b1a4c5d69b5822
Harshit <gharshit89@gmail.com>
2023-01-27 02:19:27 +0530

    update

--- Java Program/src/com/company/DSA/LinkedList/InsertAtEnd.java
+++ Java Program/src/com/company/DSA/LinkedList/InsertAtEnd.java
@@ -1,32 +1,67 @@
 package com.company.DSA.LinkedList;
 
 public class InsertAtEnd {
-static class Node{
-    int data;
-    Node next;
-    Node(int x){
-        data=x;
-        next=null;
+//static class Node{
+//    int data;
+//    Node next;
+//    Node(int x){
+//        data=x;
+//        next=null;
+//    }
+//
+//    static Node insertEnd(Node head,int a){
+//        Node temp=new Node(a);
+//        if(head==null){
+//            return temp;
+//        }
+//        else{
+//            Node curr =head;
+//            while(curr.next!=null){
+//                curr=curr.next;
+//            }
+//            curr.next=temp;
+//            return head;
+//        }
+//    }
+//    public static void main(String[] args) {
+//        Node head = new Node(10);
+//        head.next=new Node(20);
+//        System.out.println( insertEnd(head,52));
+//}
+//}
+
+    static class Node{
+        int data;
+        Node next;
+        Node(int x ){
+            data=x;
+            next=null;
+        }
     }
 
-    static Node insertEnd(Node head,int a){
+    public static Node insertAtEnd(Node head,int a){
         Node temp=new Node(a);
         if(head==null){
-            return temp;
-        }
-        else{
-            Node curr =head;
-            while(curr.next!=null){
-                curr=curr.next;
-            }
-            curr.next=temp;
+            head=temp;
             return head;
         }
+        Node curr=head;
+        while(curr.next!=null){
+            curr=curr.next;
+        }
+        curr.next=temp;
+        return head;
     }
+
+
     public static void main(String[] args) {
         Node head = new Node(10);
-        head.next=new Node(20);
-        System.out.println( insertEnd(head,52));
-}
-}
+        head.next = new Node(20);
+        insertAtEnd(head, 52);
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data + " ");
+            curr = curr.next;
+        }
+    }
 }
